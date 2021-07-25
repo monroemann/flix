a524f6deff4dbc06687164e253dae11159f6bfe9
Monroe Mann <findingsummer@aol.com>
2021-07-25 17:42:14 -0400

    more features

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
  
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root "movies#index"
@@ -7,5 +8,9 @@ Rails.application.routes.draw do
   resources :movies do
      resources :reviews
   end
+
+  get "signup" => "users#new"
+
+  resource :session, only: [:new, :create, :destroy]
 
 end
