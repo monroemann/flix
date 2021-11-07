2e38023c13bf3726e0951a22ba52ba5703a1a58f
Monroe Mann <findingsummer@aol.com>
2021-11-07 09:55:18 -0500

    Added fave button

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
 
   has_many :reviews, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :favorite_movies, through: :favorites, source: :movie
 
   validates :name, presence: true;
   validates :email, format: { with: /\S+@\S+/ },
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
  
-  resources :favorites
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root "movies#index"
 
   resources :movies do
-     resources :reviews
+    resources :reviews
+    resources :favorites, only: [:create, :destroy]
   end
 
   get "signup" => "users#new"
