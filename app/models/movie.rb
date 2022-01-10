c95d456ba9028771c17ece6baeff0b7db7862398
Monroe Mann <findingsummer@aol.com>
2022-01-10 18:47:39 -0500

    Fixed the slug bug

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -11,14 +11,13 @@ class Movie < ApplicationRecord
 	has_many :genres, through: :characterizations
 
 
-	validates :title, :released_on, :duration, presence: true
+	validates :title, :released_on, :duration, presence: true, uniqueness: true
 	validates :description, length: { minimum: 25 }
 	validates :total_gross, numericality: { greater_than_or_equal_to: 0 }
 	validates :image_file_name, format: {
 		with: /\w+\.(jpg|png)\z/i,
 		message: "must be a JPG or PNG image"
 	}
-	validates :title, presence: true, uniqueness: true
 
   RATINGS = %w(G PG PG-13 R NC-17)
   validates :rating, inclusion: { in: RATINGS }
